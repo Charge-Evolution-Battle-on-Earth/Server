6e6f4a4f6d43311837f434c12ef11a9501f5c380
duddn2012 <gowoo123@naver.com>
2023-12-30 02:44:49 +0900

    feat/#12 WebSocket 기반 통신 Ready API

--- src/main/java/com/project/game/play/controller/PlayController.java
+++ src/main/java/com/project/game/play/controller/PlayController.java
@@ -1,2 +1,26 @@
-package com.project.game.play.controller;public class PlayController {
+package com.project.game.play.controller;
+
+import com.project.game.play.dto.PlayReadyRequest;
+import com.project.game.play.dto.PlayReadyResponse;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PlayController {
+
+    @MessageMapping("TTT")
+    @SendTo("/topic/message")
+    public String greeting(String message){
+        return "test";
+    }
+
+    @MessageMapping("ready/{matchId}")
+    @SendTo("/topic/{matchId}/message")
+    public PlayReadyResponse ready(@DestinationVariable String matchId, PlayReadyRequest playReadyRequest){
+        System.out.println("matchId = " + matchId + ", playReadyRequest = " + playReadyRequest);
+        System.out.println(playReadyRequest.getReadyStatus() + playReadyRequest.getMatchId());
+        return new PlayReadyResponse(playReadyRequest.getMatchId(), !playReadyRequest.getReadyStatus(),true);
+    }
 }
--- src/main/java/com/project/game/play/dto/PlayReadyRequest.java
+++ src/main/java/com/project/game/play/dto/PlayReadyRequest.java
@@ -1,2 +1,10 @@
-package com.project.game.play.dto;public class PlayReadyRequest {
+package com.project.game.play.dto;
+
+import lombok.Getter;
+
+@Getter
+public class PlayReadyRequest {
+
+    String matchId;
+    Boolean readyStatus;    //true:준비완료, false:준비 미완료
 }

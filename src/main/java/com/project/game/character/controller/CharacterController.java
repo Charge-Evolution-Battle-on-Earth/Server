c51c51f5be150841e4030d28a3b839b67690a880
duddn2012 <gowoo123@naver.com>
2024-01-17 12:40:34 +0900

    github action test

--- src/main/java/com/project/game/character/controller/CharacterController.java
+++ src/main/java/com/project/game/character/controller/CharacterController.java
@@ -6,12 +6,8 @@ import com.project.game.character.service.CharacterService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,16 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/characters")
 public class CharacterController {
 
+    //github action test
     private final CharacterService characterService;
 
     @GetMapping
-    private ResponseEntity<CharacterInfoGetResponse> getCharacterInfo(@AuthenticationPrincipal Long characterId){
+    private ResponseEntity<CharacterInfoGetResponse> getCharacterInfo(
+        @AuthenticationPrincipal Long characterId) {
         CharacterInfoGetResponse response = characterService.getCharacterInfo(characterId);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/skills")
-    private ResponseEntity<List<CharacterSkillGetResponse>> getCharacterSkill(@AuthenticationPrincipal Long characterId){
+    private ResponseEntity<List<CharacterSkillGetResponse>> getCharacterSkill(
+        @AuthenticationPrincipal Long characterId) {
         List<CharacterSkillGetResponse> response = characterService.getCharacterSkills(characterId);
         return ResponseEntity.ok(response);
     }

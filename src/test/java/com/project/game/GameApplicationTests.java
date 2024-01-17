99ed8c724c66089349dd0bae57211aaf72433dfe
duddn2012 <gowoo123@naver.com>
2024-01-17 14:59:27 +0900

    github action test3

--- src/test/java/com/project/game/GameApplicationTests.java
+++ src/test/java/com/project/game/GameApplicationTests.java
@@ -1,13 +1,8 @@
 package com.project.game;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GameApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }

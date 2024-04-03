21b233c727cfc4d06e0ed52ee101cb689e552194
고영우 <31121731+duddn2012@users.noreply.github.com>
2024-04-04 02:21:32 +0900

    develop #42 #51 (#55)
    * fix/#42 WebSocket Connection 중 발생하는 예외에 대한 AOP 기반 처리
    
    * fix/#51 모든 API가 Response Message를 반환하도록 수정 & WebSocket 요청 시 방 참여자가 아닐 경우 체크 로직 추가
    
    * feat/#42 예외 처리 메세지 직관적으로 이해 가능하도록 수정

--- src/main/java/com/project/game/common/config/AuthenticationConfig.java
+++ src/main/java/com/project/game/common/config/AuthenticationConfig.java
@@ -1,5 +1,6 @@
 package com.project.game.common.config;
 
+import com.project.game.common.aop.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
---
+++ src/main/java/com/project/game/websocket/exception/WebSocketSessionInvalidException.java
@@ -0,0 +1,15 @@
+package com.project.game.websocket.exception;
+
+import java.util.List;
+
+public class WebSocketSessionInvalidException extends WebSocketException {
+
+    public WebSocketSessionInvalidException(List<Long> playerIds) {
+        super("WebSocketSession Invalid", playerIds);
+    }
+
+    public WebSocketSessionInvalidException(Long matchId, List<Long> playerIds) {
+        super("WebSocketSession Invalid [matchId]: " + matchId, playerIds);
+    }
+
+}

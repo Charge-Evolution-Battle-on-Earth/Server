f4d7103acc743cf05f7c67038c7ec0771a083d71
duddn2012 <gowoo123@naver.com>
2024-04-12 17:02:59 +0900

    feat/#42 Blocking 방식으로 WebSocketSession 동시성 문제 해결

--- src/main/java/com/project/game/websocket/WebSocketSessionManager.java
+++ src/main/java/com/project/game/websocket/WebSocketSessionManager.java
@@ -9,25 +9,23 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
-
-// TODO Thread 정보로 session을 식별하는 Thread Local를 통해서 Thread ID마다 Connection ID를 담아둬서 Connection ID를 활용하자.
 @Component
 public class WebSocketSessionManager {
 
     private Map<Long, WebSocketSession> webSocketSessionMap = new ConcurrentHashMap<>();
 
-    public void addWebSocketSessionMap(Long key, WebSocketSession socketSession) {
+    public synchronized void addWebSocketSessionMap(Long key, WebSocketSession socketSession) {
         if (!validateSession(socketSession)) {
             throw new WebSocketSessionInvalidException();
         }
         this.webSocketSessionMap.put(key, socketSession);
     }
 
-    public void removeWebSocketSessionMap(Long key) {
+    public synchronized void removeWebSocketSessionMap(Long key) {
         this.webSocketSessionMap.remove(key);
     }
 
-    public void sendMessage(String message, List<Long> keys) throws IOException {
+    public synchronized void sendMessage(String message, List<Long> keys) throws IOException {
         for (Long key : keys) {
             WebSocketSession socketSession = this.webSocketSessionMap.get(key);
             if (validateSession(socketSession)) {
@@ -36,7 +34,7 @@ public class WebSocketSessionManager {
         }
     }
 
-    public boolean validateSession(WebSocketSession socketSession) {
+    public synchronized boolean validateSession(WebSocketSession socketSession) {
         return socketSession != null && socketSession.isOpen();
     }
 }

532f263feea8a65ebc2e92942a19052e66ad7b96
duddn2012 <gowoo123@naver.com>
2024-04-14 00:33:39 +0900

    bug/#72 인벤토리 모든 사용자가 공유하는 쿼리 수정

--- src/main/java/com/project/game/character/repository/CharacterItemRepository.java
+++ src/main/java/com/project/game/character/repository/CharacterItemRepository.java
@@ -12,7 +12,7 @@ public interface CharacterItemRepository extends JpaRepository<CharacterItem, Lo
         + "FROM character_item ci "
         + "JOIN items i on ci.item_id = i.item_id "
         + "JOIN item_type it on i.item_type_id = it.item_type_id "
-        + "WHERE it.item_type_id = :itemTypeId"
+        + "WHERE it.item_type_id = :itemTypeId "
         + "AND ci.character_id = :characterId"
         , nativeQuery = true)
     List<CharacterItem> getInventoryList(@Param("characterId") Long characterId,
